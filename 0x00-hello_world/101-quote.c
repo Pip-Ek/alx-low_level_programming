f1a602ef0b5d4da0594ef2cc2dde53ba4be7f987
Pip-Ek <ekundayot0@gmail.com>
2022-03-10 19:10:50 -0800

    Print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, string, strlen(string));
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
 	return (1);
 }
