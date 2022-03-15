431dc7faec3e2dd99734dd29051422ada9d1ae73
Pip-Ek <ekundayot0@gmail.com>
2022-03-15 07:12:10 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i,s;
+	
+	s = sizeof(str) / sizeof(int);
+	
+	for (i = 0; i < s; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
