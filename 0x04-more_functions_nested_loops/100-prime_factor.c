951d0453619902d09dadd9cd39bd506217d922bc
Pip-Ek <ekundayot0@gmail.com>
2022-03-17 20:01:12 -0700

    Print biggest prmie factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints largest prime factor
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, f;
+
+	n = 612852475143;
+	for (f = 2; f <= n/2; fp++)
+	{
+		if (n % f == 0)
+		{
+			n /= f;
+			f--;
+		}
+	}
+	printf("%ld\n", f);
+	return (0);
+}
