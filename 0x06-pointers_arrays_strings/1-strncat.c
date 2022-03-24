b89390ff7db525115137d548a0c0143d310ca6c0
Pip-Ek <ekundayot0@gmail.com>
2022-03-24 12:10:06 -0700

    Concatenate till n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer of dest.
+ */
+char *_strncat(char *dest, char *src)
+{
+	int count1 = 0;
+	int count2 = 0;
+
+	while (*(dest + count1) != '\0')
+	{
+		count1++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count1) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count1++;
+		count2++;
+	}
+	return (dest);
+}
