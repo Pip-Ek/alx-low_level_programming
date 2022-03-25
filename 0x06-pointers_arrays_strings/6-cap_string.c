08b23fd3e62924971a81d5895a2a5df6bcc2870c
Pip-Ek <ekundayot0@gmail.com>
2022-03-24 20:37:24 -0700

    Capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,22 @@
 
 char *cap_string(char *s)
 {
-	int i, count = 0;
-	int seps[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, j, f;
+	char seps[] = {' ', '\t', '\n', ',', ';', '.'. '!', '?', '"', '(', ')', '{', '}'};
 
-
-	if(*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	f = 'A' - 'a';
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] += f;
+	for (i = 0; s[i]; i++)
 	{
-		for (i = 0; i < 13; i++)
+		for (j = 0; j < 13; j++)
 		{
-			if (*(s + count) == seps[i])
+			if (s[i] == seps[j])
 			{
-				if ((*(s + (count + 1)) >= 97 && (*(s + (count + 1)) <= 122)))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] += f;
 			}
 		}
-		count++;
 	}
 	return (s);
 }
