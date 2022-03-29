833621b9a237d818028e46e26cc5df2025941ac2
Pip-Ek <ekundayot0@gmail.com>
2022-03-28 23:27:49 -0700

    Substringging

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == '\0' || needle[j] != haystack[i])
+			if (haystack[i + j] == '\0' || needle[j] != haystack[i + j])
 				break;
 		}
 		if (needle[j] == '\0')
