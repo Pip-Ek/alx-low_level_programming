a2a821822816624fa75d0c8b20d90fbaacf93ff7
Pip-Ek <ekundayot0@gmail.com>
2022-03-28 22:27:49 -0700

    Memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: bytes filled.
  * Return: the pointer to dest.
  */
-char *-memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
