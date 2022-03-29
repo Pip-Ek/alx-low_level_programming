025b42e1bf8c54f26d9db663bedfa75ed2f63cda
Pip-Ek <ekundayot0@gmail.com>
2022-03-28 23:34:29 -0700

    Chessy chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: a 2d array of characters
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 0)
+				_putchar('\n');
+		}
+	}
+}
