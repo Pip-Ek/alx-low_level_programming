54e041c6a6b4973106c714fabef0f459c0d16442
Pip-Ek <ekundayot0@gmail.com>
2022-03-28 23:16:12 -0700

    Substringging

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: string to search
+ * @needle: string to search for
+ * Return: Pointer to first occurence or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == '\0' || needle[j] != haystack[i])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (NULL);
+}
